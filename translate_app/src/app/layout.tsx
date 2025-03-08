17f9762182f8d78b7e6e9f4de18266a17214d60f
mariussgal <marius.gal@esme.fr>
2025-03-08 13:02:39 +0100

    preview banner

--- translate_app/src/app/layout.tsx
+++ translate_app/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./NavigationBar";
+import Image from 'next/image';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,7 +17,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Translation Tool",
-  description: "English-French translation tool for TOEIC preparation",
+  description: "English-French translation tool",
+
 };
 
 export default function RootLayout({
@@ -29,6 +31,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+      <div className="w-full bg-gray-800 flex justify-center items-center py-2">
+          <Image 
+            src="../public/preview_banner.png" 
+            alt="Preview Version" 
+            width={1000} 
+            height={50} 
+            className="h-auto max-h-12 w-auto"
+            priority
+          />
+        </div>
+
+
         <Navbar />
         {children}
       </body>
